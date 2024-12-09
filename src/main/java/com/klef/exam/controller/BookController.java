9bf6f5b2e58eb6c726a2c66d76c58c22f18c02cd
klu2200031031 <147605042+klu2200031031@users.noreply.github.com>
2024-12-09 21:16:52 +0530

    exam

--- src/main/java/com/klef/exam/controller/BookController.java
+++ src/main/java/com/klef/exam/controller/BookController.java
@@ -2,6 +2,7 @@ package com.klef.exam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,8 @@ public class BookController {
 	@Autowired
 	private Bookservice bookservice;
 	
-	@PutMapping("update")
-	public String updateUser(@RequestBody Book book) {
+	@PutMapping("/{id}")
+	public String updateUser(@RequestBody Book book,@PathVariable("Id") int id) {
 		return bookservice.updateBook(book);
 	}
 }
